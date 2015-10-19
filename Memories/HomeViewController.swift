c666a9264f832a01f9652c42b1cd29d613bd8d8d
michelloj <mchlljy@mit.edu>
2015-10-19 02:17:56 -0400

    hamburger view and cleanup

--- Memories/HomeViewController.swift
+++ Memories/HomeViewController.swift
@@ -8,19 +8,39 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController /*UITableViewDataSource, UITableViewDelegate */ {
 
     @IBOutlet weak var leftButton: UIButton!
 
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var homeTableView: UITableView!
     @IBOutlet weak var memNavBar: UINavigationBar!
+    
+    let sectionHeaders = [
+        "",
+        "Distance",
+        "Sort By",
+        "Category"
+    ]
+    
+    var sectionRowCounts = [
+        1,
+        1,
+        1,
+        6 // show a few first, then potentially expand
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         leftButton.setImage(UIImage(named: "Menu"), forState: .Normal)
-        leftButton.frame = CGRectMake(0,0,20,20)
+        leftButton.frame = CGRectMake(0,0,25,25)
+        
+        rightButton.setImage(UIImage(named: "Logout"), forState: .Normal)
+        rightButton.frame = CGRectMake(0,0,25,25)
+        
+        memNavBar.tintColor = UIColor.whiteColor()
         
         //leftButton.addTarget(self, action: Selector("action"), forControlEvents: .TouchUpInside)
         
@@ -30,9 +50,29 @@ class HomeViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        return
     }
+//    
+//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        //return
+//    }
+//    
+//    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        //return
+//    }
+//    
+//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//        //return
+//    }
+//    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        //return nil
+//    }
+//
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        //return 0
+//    }
     
-
 
     /*
     // MARK: - Navigation
